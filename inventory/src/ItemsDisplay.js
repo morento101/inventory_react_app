b08cab65c22ed47a950e6430279ce0f215afabd5
morento101 <tlcees@gmail.com>
2021-12-08 13:33:48 -0800

    styled table for items

--- inventory/src/ItemsDisplay.js
+++ inventory/src/ItemsDisplay.js
@@ -1,17 +1,31 @@
 function ItemsDispaly(props) {
   return (
     <div>
-      {props.items.map((item) => {
-        return (
-          <div>
-            <p>Id: {props.items.indexOf(item) + 1}</p>
-            <p>Name: {item.name}</p>
-            <p>Type: {item.type}</p>
-            <p>Price: {item.price}</p>
-            <p>Brand: {item.brand}</p>
-          </div>
-        );
-      })}
+      <div className="title">List of Items</div>
+      <table className="content-table">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Type</th>
+            <th>Price</th>
+            <th>Brand</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.items.map((item) => {
+            return (
+              <tr>
+                <td>{props.items.indexOf(item) + 1}</td>
+                <td>{item.name}</td>
+                <td>{item.type}</td>
+                <td>{item.price}</td>
+                <td>{item.brand}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
